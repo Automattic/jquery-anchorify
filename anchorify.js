11d68d9557f88b1c73de3fbd17e01dea8bdd9e23
Borislav Kosun <kosun.b@yandex.ua>
2017-06-16 21:40:30 +0300

    Fixed bug when working with Cyrillic characters

--- anchorify.js
+++ anchorify.js
@@ -35,7 +35,7 @@
              .trim()
              .replace(/\s+/g, '-')     // Replace spaces with -
              .replace(/&/g, '-and-')   // Replace & with 'and'
-             .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+             .replace(/[^\w\-а-я]+/g, '') // Remove all non-word chars
              .replace(/\-\-+/g, '-');  // Replace multiple - with single -
 
         e.attr('id', id);
